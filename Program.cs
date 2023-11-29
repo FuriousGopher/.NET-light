566a9e638a96d4516d6f4bcb3bc2f7f2b1702f1d
FuriousGopher <alkazavr94@gmail.com>
2023-11-29 22:36:58 +0100

    int commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Console_.NET
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+        }
+    }
+}
